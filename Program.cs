688a03257c18c68226d39b06ef0208e985fa1069
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-15 23:59:29 -0300

    ejercicios terminados

--- Program.cs
+++ Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mundo, con C#");
+﻿
+
+Console.WriteLine("Quitar los comentarios a los ejercicios");
+//---Ejercicio 1-----------------------------------------------
+// Console.WriteLine("----Ejercicio 1----");
+// Console.WriteLine("Ingrese sus datos");
+
+// Console.Write("Nombre: ");
+// string nombre = Console.ReadLine();
+
+// Console.Write("Apellido: ");
+// string apellido = Console.ReadLine();
+
+// Console.Write("Edad: ");
+// int edad = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Sabes programar: ");
+// string programar = Console.ReadLine();
+
+// Console.Write("Estado Civil: ");
+// string estado_civil = Console.ReadLine();
+
+// Console.WriteLine("----Resultados-----");
+
+// Console.WriteLine("Hola " + nombre + " " + apellido);
+// Console.WriteLine("Tienes la edad de " + edad);
+// Console.WriteLine("Sabes programar " + programar);
+// Console.WriteLine("Estas " + estado_civil);
+
+
+//----Ejercicio 2----------------------------
+
+// //Coche
+// int puertas = 4;
+// int ruedas = 4;
+// string marca = "Ford";
+// bool itv = true;
+// Console.WriteLine("---Coche---");
+// Console.Write("Puertas: " + puertas);
+// Console.Write(" Ruedas: " + ruedas);
+// Console.Write(" Marca: " + marca);
+// Console.WriteLine(" ITV: " + itv);
+
+
+
+
+// //Mesa
+// float peso = 2.2f;
+// float largo = 2.5f;
+// string material = "caoba";
+// string color = "marron";
+// Console.WriteLine("----Mesa----");
+// Console.Write("Peso: " + peso);
+// Console.Write(" Largo: " + largo);
+// Console.Write(" Material: " + material);
+// Console.WriteLine(" Color: " + color);
+
+
+//----Ejercicio 2 --------------------
+
+Console.WriteLine((20 >= 18));
+char c = 'a';
+Console.WriteLine((c == 'a'));
+bool res_and = true && true;
+Console.WriteLine(res_and);
+bool res_or = true || false;
+Console.WriteLine(res_or);
+
+
+
+
+
