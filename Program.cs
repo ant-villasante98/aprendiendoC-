5b89507dd10b9d39e6c9688049d78e0dde74e6a3
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-17 22:14:52 -0300

    ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mundo, con C#");
+﻿ClientePractica client = new ClientePractica("Antonio Villasante", "24234-432", "Beccar Varela", "example@email.com", true);
+Console.WriteLine(client.ToString());
+public struct ClientePractica
+{
+
+    string nombre;
+    string telefono;
+    string direccion;
+    string email;
+    bool nuevo;
+
+    public ClientePractica(string nom, string tel, string dirc, string em, bool nue)
+    {
+        nombre = nom;
+        telefono = tel;
+        direccion = dirc;
+        email = em;
+        nuevo = nue;
+    }
+    public override string ToString() => $"Cliente {nombre}, telefono: {telefono}, direccion: {direccion}, email: {email} , nuevo: {nuevo}";
+}
